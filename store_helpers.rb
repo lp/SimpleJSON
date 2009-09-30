ad92e501de315cfb7e8d52e84cb9e89d8717ff2e
lp <lp@spiralix.org>
2009-09-30 13:35:26 -0400

    module CrashProof

--- store_helpers.rb
+++ store_helpers.rb
@@ -24,4 +24,33 @@ class SimpleJSON
 		end
 	end
 	
+	# ///////////////////////////////////////////////////////////////////////////////////////
+	# 
+	# Author:: lp (mailto:lp@spiralix.org)
+	# Copyright:: 2009 Louis-Philippe Perron - Released under the terms of the MIT license
+	# 
+	# :title:SimpleJSON::Body
+	module Body
+		def self.get(env)
+			data =  env['rack.input'].read
+			return data
+		end
+	end
+
+	# ///////////////////////////////////////////////////////////////////////////////////////
+	# 
+	# Author:: lp (mailto:lp@spiralix.org)
+	# Copyright:: 2009 Louis-Philippe Perron - Released under the terms of the MIT license
+	# 
+	# :title:SimpleJSON::CrashProof
+	module CrashProof
+		def self.wrap(&block)
+			begin
+				block.call
+			rescue
+				[200, {	'Content-Type' => 'application/json' }, "{\"ERROR\":{\"class\":\"#{$!.class}\",\"message\":\"#{$!.message}\"}}"]
+			end
+		end
+	end
+	
 end
